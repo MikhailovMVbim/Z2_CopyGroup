a4976c2b34f66933d39fc36bef5aa108a13a2499
Sirius <Sirius@DESKTOP-77RB5UD>
2022-02-09 06:59:00 +0300

    Добавьте файлы проекта.

---
+++ CopyGroup.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2_CopyGroup
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CopyGroup : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Получаем доступ к Revit, активному документу, базе данных документа
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // выбор группы
+            Reference reference = uidoc.Selection.PickObject(ObjectType.Element);
+            Element element = doc.GetElement(reference);
+            Group group = element as Group;
+
+            // выбор точки
+            XYZ point = uidoc.Selection.PickPoint("Укажите точку вставки");
+
+            //размещение группы
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("Копирование группы");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            transaction.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
